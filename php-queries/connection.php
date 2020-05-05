1118c7bcba07d9719127c96ac514cf5e2f0a86b6
vatsa287 <i.mnshreevatsa@gmail.com>
2020-05-05 20:11:12 +0530

    Establish Connection with DB
    Signed-off-by: vatsa287 <i.mnshreevatsa@gmail.com>

---
+++ php-queries/connection.php
@@ -0,0 +1,17 @@
+<?php
+echo "Response Messages: \n";
+
+$conn = mysqli_connect('localhost','root','');
+mysqli_select_db($conn,'product');
+
+if($conn)		//if connection is successfull
+{
+	echo "Database Connected! \n";
+	echo "Database Name:Demo! \n";
+	echo "Database Username:Root! ";
+}
+else			//if connection is not successfull
+{
+	echo "Database Not Connected!";
+}
+?>
