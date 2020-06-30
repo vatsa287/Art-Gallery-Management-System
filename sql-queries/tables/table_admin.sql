985b5ea8c0c4ddbd7b0a140402d8de6d8dd37ced
vatsa287 <i.mnshreevatsa@gmail.com>
2020-06-30 11:38:17 +0530

    Sql query for table_admin

--- sql-queries/tables/table_admin.sql
+++ sql-queries/tables/table_admin.sql
@@ -1,4 +1,11 @@
+--
+-- Table structure for table `table_admin`
+--
+
+DROP TABLE IF EXISTS `table_admin`;
 CREATE TABLE IF NOT EXISTS `table_admin` (
   `admin_Name` varchar(100) NOT NULL,
   `admin_Password` varchar(100) NOT NULL
-)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+COMMIT;
